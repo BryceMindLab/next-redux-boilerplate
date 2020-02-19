f905f441f40627949dec7efbd41a6dda64e178cf
Bryce Doganer <bryce.doganer@gmail.com>
2020-02-19 09:33:52 -0500

    [feat] init redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import App from 'next/app';
 import React from 'react';
+
 import { ThemeProvider } from 'styled-components';
+
+import { Provider } from 'react-redux';
+import store from '../store';
 
 const theme: Theme = {
 	colors: {
@@ -13,9 +17,11 @@ export default class MyApp extends App {
 	render() {
 		const { Component, pageProps } = this.props;
 		return (
-			<ThemeProvider theme={theme}>
-				<Component {...pageProps} />
-			</ThemeProvider>
+			<Provider store={store}>
+				<ThemeProvider theme={theme}>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</Provider>
 		);
 	}
 }
